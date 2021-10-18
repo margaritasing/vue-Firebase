41d51b2a3bdb9c51320d0538afb0e76a7e617f04
margaritasing <61855440+margaritasing@users.noreply.github.com>
2021-10-18 16:43:17 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="nav">
+
+    <nav-bar />
+
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-view/>
+  </div>
+  
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue"
+
+export default {
+  components: { NavBar },
+  
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="lighten-2">
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Apfel-Urban</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Sass</a></li>
+        <li><a href="#">Components</a></li>
+        <li><a href="#">Javascript</a></li>
+        <li><a href="#">Mobile</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="#">Sass</a></li>
+    <li><a href="#">Components</a></li>
+    <li><a href="#">Javascript</a></li>
+    <li><a href="#">Mobile</a></li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted(){
+    M.AutoInit();
+  },
+
+}
+</script>
+
+<style>
+
+</style>
