cf5708bf1755f36a924dc46a168d835d871600c1
margaritasing <61855440+margaritasing@users.noreply.github.com>
2021-10-22 23:18:42 -0300

    agregando el componente de resgistro

---
+++ src/components/Register.vue
@@ -0,0 +1,42 @@
+<template>
+
+<div class="container">
+    <div class="row">
+    <form @submit.prevent=""   class="col s12">
+        <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Correo</label>
+        </div>
+      </div>
+     <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Contrasena</label>
+        </div>
+      </div>
+       <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+
+      <button class="waves-effect waves-light btn" type="submit">Registrar</button>
+      
+    </form>
+  </div>
+</div>
+        
+ 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <register />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Register from '../components/Register.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Register
   }
 }
 </script>
