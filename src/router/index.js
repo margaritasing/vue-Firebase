f4f22a4eea67be655e0963ed62f58d2bcec39d72
margaritasing <61855440+margaritasing@users.noreply.github.com>
2021-10-23 17:25:56 -0300

    agrego proteccion a las rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,57 +1,67 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [{
-        path: '/',
-        name: 'Home',
-        component: Home
+        path: "/",
+        name: "Home",
+        component: Home,
     },
     {
-        path: '/login',
-        name: 'Login',
+        path: "/login",
+        name: "Login",
         // route level code-splitting
-        // this generates a separate chunk (login.[hash].js) for this route
+        // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import ( /* webpackChunkName: "login" */ '../views/Login.vue')
+            import ( /* webpackChunkName: "login" */ "../views/Login.vue"),
     },
-
     {
-        path: '/proyectos',
-        name: 'Proyectos',
+        path: "/proyectos",
+        name: "Proyectos",
         // route level code-splitting
-        // this generates a separate chunk (proyectos.[hash].js) for this route
+        // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import ( /* webpackChunkName: "proyectos" */ '../views/Proyectos.vue')
+            import ( /* webpackChunkName: "proyectos" */ "../views/Proyectos.vue"),
+        meta: { protect: true },
     },
-
     {
-        path: '/reg-projects',
-        name: 'Created',
+        path: "/reg-projects",
+        name: "Created",
         // route level code-splitting
         // this generates a separate chunk (reg-projects.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import ( /* webpackChunkName: "reg-projects" */ '../views/Created.vue')
+            import ( /* webpackChunkName: "reg-projects" */ "../views/Created.vue"),
+        meta: { protect: true },
     },
-
     {
-        path: '/edit-project',
-        name: 'Edit',
+        path: "/edit-project/:id",
+        name: "Edit",
         // route level code-splitting
         // this generates a separate chunk (edit-projects.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import ( /* webpackChunkName: "edit-projects" */ '../views/Edit.vue')
+            import ( /* webpackChunkName: "edit-projects" */ "../views/Edit.vue"),
+        meta: { protect: true },
     },
-
-
-]
+];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
-})
+    routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+    if (to.meta.protect) {
+        if (localStorage.getItem("user")) {
+            next();
+        } else {
+            next("/login");
+        }
+    } else {
+        next();
+    }
+});
+
+export default router;
